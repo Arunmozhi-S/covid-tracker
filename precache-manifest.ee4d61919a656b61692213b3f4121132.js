cb05b42735507613e3f9965b77b4a29b55f6c9ef
Arunmozhi_S <arunmozhi77784@gmail.com>
2020-09-26 17:43:02 +0530

    Updates

---
+++ precache-manifest.ee4d61919a656b61692213b3f4121132.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4357506e9aba532e36abc5e602781a4e",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "6cd8949772cbcaded1ae",
+    "url": "/covid-tracker/static/css/main.355def48.chunk.css"
+  },
+  {
+    "revision": "2ea27f031819a0455595",
+    "url": "/covid-tracker/static/js/2.ef8f5edb.chunk.js"
+  },
+  {
+    "revision": "bdb08df6d197ae36b9622efbd218f2e4",
+    "url": "/covid-tracker/static/js/2.ef8f5edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd8949772cbcaded1ae",
+    "url": "/covid-tracker/static/js/main.567eda8d.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/covid.d7265326.png"
+  }
+]);
